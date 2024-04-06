c42257c4905b9983d4dd498729ca8ab957657481
Maciej Kowalski <maciej.ko1444@gmail.com>
2024-04-06 13:12:45 +0200

    2.18 done

--- 2.18/main.cpp
+++ 2.18/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+/*Dla losowego całkowitego n z zakresu <5;12> narysuj „kwadrat” składający się
+z n wierszy i n kolumn, którego krawędź to znak '#', a wnętrze jest puste (spacje).*/
 int main(){
     srand(time(nullptr));
     int random = rand() % 8 + 5;
+    std::cout << "Wylosowano liczbę: " << random << "\n";
     for(int i {}; i < random; i++){
         for(int j {}; j < random; ++j){
             if(i == 0 || i == (random-1) || j == 0 || j == (random-1)){
