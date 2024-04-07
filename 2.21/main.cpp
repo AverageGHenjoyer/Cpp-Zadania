6a46a248a7b42395172f56206bfe7191c65c633f
Maciej Kowalski <maciej.ko1444@gmail.com>
2024-04-07 21:02:03 +0200

    2.21 done

--- 2.21/main.cpp
+++ 2.21/main.cpp
@@ -4,7 +4,7 @@ int main(){
     srand(time(nullptr));
 
     int random = rand() % 8 + 5;
-    int rzad = rand() % (random-1) + 1;
+    int rzad = rand() % (random-1) + 2;
     std::cout << "Wylosowano: \n\trandom: " << random << "\n\trzad i kolumna: " << rzad << "\n";
     for(int i {1}; i <= random; i++){
         for(int j {1}; j <= random; j++){
@@ -15,4 +15,22 @@ int main(){
         }
         std::cout << "\n";
     }
+    
+    std::cout << "\n";
+
+    for(int i {1}; i <= random; i++){
+        for(int j {1}; j <= random; j++){
+            if(j == rzad && i == rzad){
+                std::cout << "@";
+            }else if((i == 1 || i == random) || (j == 1 || j == random))
+                std::cout << "#";
+            else 
+            {
+                std::cout << " ";
+            }
+        }
+        std::cout << "\n";
+    }
+
+
 }
