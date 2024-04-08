b4ca1d404f9b195f692389962707c22302871c23
Maciej Kowalski <maciej.ko1444@gmail.com>
2024-04-08 21:56:52 +0200

    added task description

--- 2.29/main.cpp
+++ 2.29/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
+/*Utwórz funkcję obliczającą wartość funkcji liniowej dla podanego x i podanych
+współczynników (wszystko double). Następnie utwórz losową tablicę dwuwymia-
+rową [100] [2] (sto wierszy, dwie kolumny) i potraktuj wiersz jak punkt w układzie
+współrzędnych. Punkty to losowe liczby z zakresu od –10 do 10 z dokładnością
+do jednego miejsca po przecinku. Dla funkcji liniowej 2x+3 sprawdź, które z punk-
+tów są nad jej wykresem, które są pod wykresem, a które leżą na nim. Podaj
+statystykę, ile punktów leży pod wykresem funkcji, ile leży nad nim i ile na linii
+utworzonej przez ten wykres. Jeżeli Twój algorytm jest dobry, to dla funkcji
+2x+3 podane punkty:
+punkty = { {0,1},{1,10},{2,7} };
+będą leżały: jeden nad wykresem funkcji liniowej, jeden pod nim, jeden na wy-
+kresie.*/
 double linear_func_calc(double a, double b, double x){
     return a*x + b;
 }
